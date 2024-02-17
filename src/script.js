f0c8321c8a783cccfcfc300315ed667a85607989
Mariana Martins <marih_ms@hotmail.com>
2024-02-17 17:40:18 -0300

    elemento com checkbox marcado agora fica riscado

--- src/script.js
+++ src/script.js
@@ -4,41 +4,67 @@ document.getElementById("botao-nova-tarefa").addEventListener("click", function(
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         let li = document.createElement("li");
-        // let botaoRemover = document.createElement("button");
         let iconeLixeira = document.createElement("img");
-        iconeLixeira.src = "../img/lixeira.png"
+        iconeLixeira.src = "../img/lixeira-x.png"
+        iconeLixeira.style.float = "right";
+        
+        // let botaoRemover = document.createElement("button");
         // botaoRemover.appendChild(iconeLixeira);
         // botaoRemover.style.float = "right";
-        iconeLixeira.style.float = "right";
-       
 
-        iconeLixeira.addEventListener("click", function() {
-            this.parentElement.remove()
+        // o span foi necessário somente por causa da função de riscar
+        // o texto dentro do <li> quando a checkbox estiver marcada
+        let span = document.createElement("span");
+        span.appendChild(document.createTextNode(novaAtividade.value));
+        
+    iconeLixeira.addEventListener("click", function() {
+        this.parentElement.remove()
+    });
+
+        checkbox.addEventListener("change", function() {
+            if(this.checked) {
+                this.nextSibling.style.textDecoration = "line-through";
+            } else {
+                this.nextSibling.style.textDecoration = "none";
+            }
         });
 
-        li.appendChild(checkbox);
-        li.appendChild(document.createTextNode(novaAtividade.value));
-        li.appendChild(iconeLixeira);
+    iconeLixeira.className ="icone-lixeira";
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(iconeLixeira);
             
         
-        let addNovaTarefa = document.getElementById("lista-tarefas");
-        addNovaTarefa.appendChild(li);
-        novaAtividade.value = "";
+    let addNovaTarefa = document.getElementById("lista-tarefas");
+    addNovaTarefa.appendChild(li);
+    novaAtividade.value = "";
 
-        checkbox.className = "checkbox";
-        botaoRemover.className ="botao-remover";
-        
+    checkbox.className = "checkbox";
     }
 });
 
 let checkboxFixo = document.getElementsByClassName("checkbox-fixo");
-let botaoRemoverFixo = document.getElementsByClassName("botao-remover-fixo");
+let botaoRemoverFixo = document.getElementsByClassName("icone-lixeira-fixa");
 
 for (let i = 0; i < botaoRemoverFixo.length; i++) {
     botaoRemoverFixo[i].addEventListener("click", function () {
         this.parentElement.remove();
+        
     });
+}
 
+let checkboxFixas = document.getElementsByClassName("checkbox-fixo")
+
+for (let i = 0; i < checkboxFixas.length; i++) {
+    checkboxFixas[i].addEventListener("change", function () {
+        if(this.checked) {
+        this.nextSibling.style.textDecoration = "line-through";
+            } else {
+                this.nextSibling.style.textDecoration = "none";
+            }
+        
+    });
 }
 
 
