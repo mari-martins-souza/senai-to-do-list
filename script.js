0acd56dffd5a739837da5cc3ce43bac80fe83fa7
Mariana Martins <marih_ms@hotmail.com>
2024-02-18 11:10:55 -0300

    upgrade de estilo e background

--- script.js
+++ script.js
@@ -5,12 +5,8 @@ document.getElementById("botao-nova-tarefa").addEventListener("click", function(
         checkbox.type = "checkbox";
         let li = document.createElement("li");
         let iconeLixeira = document.createElement("img");
-        iconeLixeira.src = "../img/lixeira-x.png"
+        iconeLixeira.src = "/img/lixeira-x.png"
         iconeLixeira.style.float = "right";
-        
-        // let botaoRemover = document.createElement("button");
-        // botaoRemover.appendChild(iconeLixeira);
-        // botaoRemover.style.float = "right";
 
         // o span foi necessário somente por causa da função de riscar
         // o texto dentro do <li> quando a checkbox estiver marcada
